5a75e370cc3835a083d54e1663a4613bd1524a07
HelloKitty <39417815+yangyang233333@users.noreply.github.com>
2024-09-13 15:18:18 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@ if [ ! -d build ]; then
     mkdir build
 fi
 
-cmake -B build -S . -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+# cmake -B build -S . -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=1
+cmake -B build -S . -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake
+
 cmake --build build
 
 ./build/cpptest
